b4c728d382c362f035800689154b8a4a1ff3dc6a
Denis <denchik7366@gmail.com>
2022-07-31 23:30:22 +0400

    added animation and decided to load fonts

--- js/script.js
+++ js/script.js
@@ -16,15 +16,9 @@ headerBurger.addEventListener('click', function(e) {
     }
 })
 
-
 function headerLogoActive() {
     return (headerLogo.classList.contains("_active"));
 }
-// function htmlScrollY() {
-//     window.addEventListener('scroll', function (event) {
-//         return this.scrollY;
-//     })
-// }
 
 window.addEventListener('scroll', function (event) {
     if (this.scrollY > 80 && !headerLogo.classList.contains('_active')) {
@@ -34,3 +28,43 @@ window.addEventListener('scroll', function (event) {
         headerLogo.style.top = '0'
     }
 })
+
+
+const animItems = document.querySelectorAll('._anim-items') // получаю все элементы с классом
+if (animItems.length > 0) {
+    window.addEventListener('scroll', animOnScroll)
+    function animOnScroll() {
+        for (let index = 0; index < animItems.length; index++) { 
+            const animItem = animItems[index]; // записываю элемент массива
+            const animItemHeight = animItem.offsetHeight; // высота элемента
+            const animItemOffset = offset(animItem).top; // позиция объекта
+            const animStart = 4; // коэффициент момента старта анимации относительно объекта типо 1/4
+
+            let animItemPoint = window.innerHeight - animItemHeight / animStart;
+
+            if (animItemHeight > window.innerHeight) { // если объект высота объекта выше высоты окна браузера
+                animItemPoint = window.innerHeight - window.innerHeight / animStart
+            }
+
+            if ((pageYOffset > animItemOffset - animItemPoint) && pageYOffset < (animItemOffset + animItemHeight)) {
+                animItem.classList.add('_active');
+            } else {
+                if (!animItem.classList.contains('_anim-no-hide')) { // чтобы не анимировался повторно
+                    animItem.classList.remove('_active');
+                }
+            }
+        } 
+    }
+    function offset(el) { // кроссбраузерное нахождение положения объекта на странице
+        const rect = el.getBoundingClientRect(),
+            scrollLeft = window.pageXOffset || document.documentElement.scrollLeft,
+            scrollTop = window.pageYOffset || document.documentElement.scrollTop;
+        return {top: rect.top + scrollTop, left: rect.left + scrollLeft}    
+    }  
+    setTimeout(() => { // задержка
+        animOnScroll();
+    }, 300);
+}
+
+
+
